9ddbc5abc874c29de76a798f2e43a5889accb770
glinka <alexandraglinka@yandex.ru>
2024-04-03 22:27:25 +0300

    проверочный запуск тестового набора

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,12 +1,15 @@
 package com.gridnine.testing;
 
+import java.util.List;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<Flight> flights = FlightBuilder.createFlights();
 
+        for (Flight flight : flights) {
+            System.out.println(flight);
+        }
     }
 }
