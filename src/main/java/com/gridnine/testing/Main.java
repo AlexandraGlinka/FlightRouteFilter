6673b3d92b18b0321214f3b7e5e98826d741865d
glinka <alexandraglinka@yandex.ru>
2024-04-04 13:12:25 +0300

    рефакторинг

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,6 +1,7 @@
 package com.gridnine.testing;
 
 import com.gridnine.testing.entity.Flight;
+import com.gridnine.testing.service.FlightFilter;
 import com.gridnine.testing.service.FlightFilterImpl;
 import com.gridnine.testing.testingData.FlightBuilder;
 
@@ -9,24 +10,18 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Flight> flights = FlightBuilder.createFlights();
-
-        System.out.println("Тестовые данные:");
-        flights.forEach(System.out::println);
-        System.out.println(separator());
+        FlightFilter unfilteredFlights = new FlightFilterImpl();
 
         System.out.println("Правило исключения 1: Вылет до текущего момента времени.");
-        List<Flight> filteredFlightsBeforeNow = new FlightFilterImpl().filterBeforeNow(flights);
-        filteredFlightsBeforeNow.forEach(System.out::println);
+        unfilteredFlights.filterBeforeNow(flights).forEach(System.out::println);
         System.out.println(separator());
 
         System.out.println("Правило исключения 2: Сегменты с датой прилёта раньше даты вылета.");
-        List<Flight> filteredFlightsArrivalBeforeDepart = new FlightFilterImpl().filterArrivalBeforeDeparture(flights);
-        filteredFlightsArrivalBeforeDepart.forEach(System.out::println);
+        unfilteredFlights.filterArrivalBeforeDeparture(flights).forEach(System.out::println);
         System.out.println(separator());
 
         System.out.println("Правило исключения 3: Перелеты, где общее время, проведённое на земле, превышает два часа.");
-        List<Flight> filteredFlightsTransferExceedsTwoHours = new FlightFilterImpl().filterTotalTransferExceedsTwoHours(flights);
-        filteredFlightsTransferExceedsTwoHours.forEach(System.out::println);
+        unfilteredFlights.filterTotalTransferExceedsTwoHours(flights).forEach(System.out::println);
         System.out.println(separator());
     }
 
